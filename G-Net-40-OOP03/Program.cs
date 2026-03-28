97f06d1264800e0fde1d4b0a5109eaabf5d3097d
Softlaptop <Softlaptop@DESKTOP-65SBLOR>
2026-03-28 19:36:35 +0200

    Add project files.

---
+++ G-Net-40-OOP03/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_Net_40_OOP03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
